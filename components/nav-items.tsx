19d9c9de462f55f33d0e6e950ef073f3795841ef
Collins Ikechukwu <collinschristroa@gmail.com>
2025-01-27 11:49:58 +0100

    refactor(components): remove unused icon imports and clean up code

--- components/nav-items.tsx
+++ components/nav-items.tsx
@@ -1,29 +1,16 @@
 "use client";
 
 import {
-  Folder,
-  Forward,
   Link2Icon,
-  MoreHorizontal,
-  Trash2,
   type LucideIcon,
 } from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 
@@ -36,7 +23,6 @@ export function NavItems({
     icon: LucideIcon;
   }[];
 }) {
-  const { isMobile } = useSidebar();
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
