41190e6028534875adfb7fd678ec9330940c1cf2
Collins Ikechukwu <collinschristroa@gmail.com>
2025-03-09 13:12:03 +0100

    feat(create_project): add metadata_uri and change project_id to String in create_project function

--- contracts/project_contract/src/create_project/create_project.rs
+++ contracts/project_contract/src/create_project/create_project.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{contract, contractimpl, Address, Env};
+use soroban_sdk::{contract, contractimpl, Address, Env, String};
 use crate::project::Project;
 
 use super::{CreateProjectEvent, CreateProjectStorage};
@@ -9,13 +9,11 @@ pub struct CreateProject;
 
 #[contractimpl]
 impl CreateProject {
-    pub fn create_project(env: Env, project_id: u64, creator: Address, funding_target: u64, milestone_count: u32) {
-        let project = Project::new(&env, project_id, creator.clone(), funding_target, milestone_count);
+    pub fn create_project(env: Env, project_id: String, creator: Address, metadata_uri: String, funding_target: u64, milestone_count: u32) {
+        let project = Project::new(&env, project_id.clone(), creator.clone(), metadata_uri, funding_target, milestone_count);
         
-        // Save to storage
         CreateProjectStorage::save(&env, &project);
 
-        // Emit event
         CreateProjectEvent::emit(&env, project_id, creator);
     }
 }
