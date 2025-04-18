a51ee9ffd8f3a390773241111a7307b061fe7cae
Benjtalkshow <chinedubenj@gmail.com>
2025-04-19 00:36:59 +0100

    fix script/utils

--- scripts/util.ts
+++ scripts/util.ts
@@ -12,7 +12,7 @@ for (const key in process.env) {
 export const loadAccount = () => {
   console.log(process.env.STELLAR_ACCOUNT)
   // This takes the secret key from SOROBAN_SECRET_KEY env-variable, so make sure you have that set.
-  exe(`stellar keys add ${process.env.STELLAR_ACCOUNT}`);
+  exe(`stellar keys add ${process.env.STELLAR_ACCOUNT || 'alice'}`);
 };
 
 // Function to execute and log shell commands
