81f8d68626c6ff0c004aa19be12e227d31d6a669
chibokaxavier <chibokaxavier@gmail.com>
2025-08-22 17:13:44 +0100

    Fix-137  Implement Responsive Projects Page at user/projects

--- app/user/projects/page.tsx
+++ app/user/projects/page.tsx
@@ -1,12 +1,11 @@
 'use client';
-
+import Projects from '@/components/Projects';
 import React from 'react';
 
 const page = () => {
   return (
-    <div className='p-8'>
-      <h1 className='text-2xl font-bold mb-4'>Projects</h1>
-      <p>Projects page content will be added here.</p>
+    <div className='bg-[#1C1C1C] p-4 sm:p-6 mx-6 rounded-[12px] flex flex-col gap-6 sm:gap-8 '>
+      <Projects />
     </div>
   );
 };
