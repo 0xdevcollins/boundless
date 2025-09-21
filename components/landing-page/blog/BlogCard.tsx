b418cc171f3158a6138474e919356f1b9be86c3a
Ekene Ngwudike <ngwudikeekene@gmail.com>
2025-09-22 00:05:20 +0100

    feat: updated the blog card with the right icon

--- components/landing-page/blog/BlogCard.tsx
+++ components/landing-page/blog/BlogCard.tsx
@@ -22,10 +22,9 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
   return (
     <Card
       key={blog.id}
-      className='w-full max-w-noneflex h-full flex-col gap-0 overflow-hidden rounded-[8px] border-[#1B1B1B] bg-[#101010] p-0 transition-colors duration-300 hover:border-[#2A2A2A]'
+      className='max-w-noneflex h-full w-full flex-col gap-0 overflow-hidden rounded-[8px] border-[#1B1B1B] bg-[#101010] p-0 transition-colors duration-300 hover:border-[#2A2A2A]'
     >
-    <CardHeader className='relative p-0 !pb-0'>
-        
+      <CardHeader className='relative p-0 !pb-0'>
         <div className='h-[214px] w-full'>
           <Image
             src={blog.image}
@@ -36,30 +35,37 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
           />
         </div>
       </CardHeader>
-      
-      <CardContent className="py-3 px-3 pb-8 border-b flex-1 border-[#2B2B2B]">
 
-        <div className="flex items-center justify-between leading-[145%] mb-3 text-xs text-[#b5b5b5] sm:text-sm">
-          <span className='inline-block rounded-[8px]  bg-[#A7F95014] px-2.5 py-2 text-sm font-medium text-[#A7F950]'>{blog.category}</span>
+      <CardContent className='flex-1 border-b border-[#2B2B2B] px-3 py-3 pb-8'>
+        <div className='mb-3 flex items-center justify-between text-xs leading-[145%] text-[#b5b5b5] sm:text-sm'>
+          <span className='inline-block rounded-[8px] bg-[#A7F95014] px-2.5 py-2 text-sm font-medium text-[#A7F950]'>
+            {blog.category}
+          </span>
           <span className='font-normal'>{blog.date}</span>
         </div>
-        <h2 className="line-clamp-2 text-[16px] leading-[145%] font-semibold text-white sm:text-base">
+        <h2 className='line-clamp-2 text-[16px] leading-[145%] font-semibold text-white sm:text-base'>
           {blog.title}
         </h2>
-        <p className="mt-3 line-clamp-3 text-sm leading-[145%] font-normal tracking-[-0.48px] text-[#B5B5B5] sm:text-base">
+        <p className='mt-3 line-clamp-3 text-sm leading-[145%] font-normal tracking-[-0.48px] text-[#B5B5B5] sm:text-base'>
           {blog.excerpt}
         </p>
       </CardContent>
 
-      <CardFooter className="pt-6 px-5 pb-5 mt-auto">
+      <CardFooter className='mt-auto px-5 pt-6 pb-5'>
         <Link
           href={`/blog/${blog.slug}`}
-          className="w-full text-right text-sm font-medium text-[#A7F950]"
+          className='flex w-full items-center justify-end gap-2 text-right text-sm font-medium text-[#A7F950]'
         >
-          Continue reading →
+          Continue reading
+          <Image
+            src='/right.svg'
+            alt='right icon'
+            width={40}
+            height={40}
+            className='inline-block w-4 pt-1'
+          />
         </Link>
       </CardFooter>
-
     </Card>
   );
 };
